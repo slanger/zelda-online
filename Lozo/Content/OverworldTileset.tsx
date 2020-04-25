9ad9d5b7bac037e9e406a8d47e69f74e08c9f08b
Mark Langer <s.mark.langer@gmail.com>
2020-04-25 17:28:55 -0400

    Add demo map via Tiled and MonoGame Extended

---
+++ Lozo/Content/OverworldTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="OverworldTileset" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="45" columns="9">
+ <image source="OverworldTileset.png" trans="008000" width="154" height="86"/>
+</tileset>
---
+++ Lozo/Content/dll/README.md
@@ -0,0 +1,2 @@
+This directory is a work-around for https://github.com/craftworkgames/MonoGame.Extended/issues/626
+based on this comment: https://github.com/craftworkgames/MonoGame.Extended/issues/626#issuecomment-583733555
