4230e0aad07be28ece4e3a30be512502999c872d
Mark Langer <s.mark.langer@gmail.com>
2020-09-19 12:06:11 -0400

    Add test for the player colliding with an immovable
    The added test only tests colliding with one immovable. I added a TODO to add more tests for testing collding with multiple immovables.

--- LozoTests/PlayerTest.cs
+++ LozoTests/PlayerTest.cs
@@ -1,6 +1,7 @@
 using Lozo;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
 using MonoGame.Extended.Tiled;
 using System;
 using Xunit;
@@ -10,9 +11,12 @@ namespace LozoTests
 {
 	public class PlayerTest : MakeConsoleWork
 	{
+		private const int ImmovableWidth = Player.Width;
+		private const int ImmovableHeight = Player.Height;
+
 		public PlayerTest(ITestOutputHelper output) : base(output) { }
 
-		private static Player MakeWorldAndPlayer()
+		private static Player MakeWorldAndPlayer(params Rectangle[] immovables)
 		{
 			var world = new World();
 			var map = new TiledMap("TestMap", Room.NumTilesWidth, Room.NumTilesHeight, Room.TileWidth, Room.TileHeight, TiledMapTileDrawOrder.RightDown, TiledMapOrientation.Orthogonal);
@@ -25,7 +29,15 @@ namespace LozoTests
 				}
 			}
 			map.AddLayer(bottomLayer);
-			var collisionLayer = new TiledMapObjectLayer(Dungeon.CollisionLayerName, new TiledMapObject[0]);
+			var collidables = new TiledMapRectangleObject[immovables.Length];
+			int i = 0;
+			foreach (Rectangle immovable in immovables)
+			{
+				// Note: Object IDs for Tiled maps start at 1, not 0.
+				collidables[i] = new TiledMapRectangleObject(i + 1, "collidable" + (i + 1), new Size2(immovable.Width, immovable.Height), new Vector2(immovable.X, immovable.Y));
+				++i;
+			}
+			var collisionLayer = new TiledMapObjectLayer(Dungeon.CollisionLayerName, collidables);
 			map.AddLayer(collisionLayer);
 			var dungeon = new Dungeon(world, map, null);
 			world.Dungeons.Add(dungeon);
@@ -34,6 +46,7 @@ namespace LozoTests
 			return player;
 		}
 
+		// This method assumes that the movement will be unhindered by any collidables.
 		private static void UpdateAndAssert(Player player, KeyboardState state, Direction? expectedDirection)
 		{
 			Rectangle oldLocation = player.Location;
@@ -184,7 +197,73 @@ namespace LozoTests
 			UpdateAndAssert(player, new KeyboardState(Keys.Left, Keys.Up, Keys.Right, Keys.Down), null);
 		}
 
-		// TODO: Test collision.
+		[Fact]
+		public void MovementWithOneImmovable()
+		{
+			Player player = MakeWorldAndPlayer();
+			Rectangle startingLocation = player.Location;
+			Rectangle startingCollider = player.WalkingCollider();
+
+			// Put an immovable below the player and check that the immovable stops the player.
+			player = MakeWorldAndPlayer(new Rectangle(startingLocation.X, startingCollider.Bottom, ImmovableWidth, ImmovableHeight));
+			Rectangle oldLocation = player.Location;
+			player.Update(new KeyboardState(Keys.Down));
+			Assert.Equal(Direction.Down, player.Direction);
+			Assert.Equal(oldLocation, player.Location);
+			// Check that the player can still move in other directions.
+			UpdateAndAssert(player, new KeyboardState(Keys.Left), Direction.Left);
+			UpdateAndAssert(player, new KeyboardState(Keys.Right), Direction.Right);
+			UpdateAndAssert(player, new KeyboardState(Keys.Up), Direction.Up);
+
+			// Same as above, but with the immovable *above* the player. Note that because of the
+			// shape of the walking collider, the immovable will actually be placed inside the
+			// player's sprite (but outside the walking collider).
+			player = MakeWorldAndPlayer(new Rectangle(startingLocation.X, startingCollider.Top - ImmovableHeight, ImmovableWidth, ImmovableHeight));
+			oldLocation = player.Location;
+			player.Update(new KeyboardState(Keys.Up));
+			Assert.Equal(Direction.Up, player.Direction);
+			Assert.Equal(oldLocation, player.Location);
+			UpdateAndAssert(player, new KeyboardState(Keys.Right), Direction.Right);
+			UpdateAndAssert(player, new KeyboardState(Keys.Left), Direction.Left);
+			UpdateAndAssert(player, new KeyboardState(Keys.Down), Direction.Down);
+
+			// Same as above, but with the immovable to the right of the player.
+			player = MakeWorldAndPlayer(new Rectangle(startingCollider.Right, startingLocation.Y, ImmovableWidth, ImmovableHeight));
+			oldLocation = player.Location;
+			player.Update(new KeyboardState(Keys.Right));
+			Assert.Equal(Direction.Right, player.Direction);
+			Assert.Equal(oldLocation, player.Location);
+			UpdateAndAssert(player, new KeyboardState(Keys.Up), Direction.Up);
+			UpdateAndAssert(player, new KeyboardState(Keys.Down), Direction.Down);
+			UpdateAndAssert(player, new KeyboardState(Keys.Left), Direction.Left);
+
+			// Same as above, but with the immovable to the left of the player.
+			player = MakeWorldAndPlayer(new Rectangle(startingCollider.Left - ImmovableWidth, startingLocation.Y, ImmovableWidth, ImmovableHeight));
+			oldLocation = player.Location;
+			player.Update(new KeyboardState(Keys.Left));
+			Assert.Equal(Direction.Left, player.Direction);
+			Assert.Equal(oldLocation, player.Location);
+			UpdateAndAssert(player, new KeyboardState(Keys.Down), Direction.Down);
+			UpdateAndAssert(player, new KeyboardState(Keys.Up), Direction.Up);
+			UpdateAndAssert(player, new KeyboardState(Keys.Right), Direction.Right);
+
+			// Now check that the player can move in the opposite direction immediately after
+			// colliding with an immovable.
+			player = MakeWorldAndPlayer(new Rectangle(startingLocation.X, startingCollider.Bottom, ImmovableWidth, ImmovableHeight));
+			player.Update(new KeyboardState(Keys.Down));
+			UpdateAndAssert(player, new KeyboardState(Keys.Up), Direction.Up);
+			player = MakeWorldAndPlayer(new Rectangle(startingLocation.X, startingCollider.Top - ImmovableHeight, ImmovableWidth, ImmovableHeight));
+			player.Update(new KeyboardState(Keys.Up));
+			UpdateAndAssert(player, new KeyboardState(Keys.Down), Direction.Down);
+			player = MakeWorldAndPlayer(new Rectangle(startingCollider.Right, startingLocation.Y, ImmovableWidth, ImmovableHeight));
+			player.Update(new KeyboardState(Keys.Right));
+			UpdateAndAssert(player, new KeyboardState(Keys.Left), Direction.Left);
+			player = MakeWorldAndPlayer(new Rectangle(startingCollider.Left - ImmovableWidth, startingLocation.Y, ImmovableWidth, ImmovableHeight));
+			player.Update(new KeyboardState(Keys.Left));
+			UpdateAndAssert(player, new KeyboardState(Keys.Right), Direction.Right);
+		}
+
+		// TODO: Add test for walking into multiple immovables.
 
 		// TODO: Test trying to walk while attacking.
 
